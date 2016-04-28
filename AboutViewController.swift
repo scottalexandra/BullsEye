aadf2b39c2e86fedf9ff99e1e2576a27aa2eda41
Alex Robinson <alexandra.scott335@gmail.com>
2016-04-28 15:01:42 -0600

    complete app, with graphics and 4s and 6s plus sizing

--- AboutViewController.swift
+++ AboutViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 
 class AboutViewController: UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    
     @IBAction func close() {
         dismissViewControllerAnimated(true, completion: nil)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            if let htmlData = NSData(contentsOfFile: htmlFile) {
+                let baseURL = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+                webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
