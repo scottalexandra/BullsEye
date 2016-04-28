c8620c8e9bb1252e2bb35e347a4e6fc5cd6f7d9d
Alex Robinson <alexandra.scott335@gmail.com>
2016-04-27 21:40:22 -0600

    completed game logic

---
+++ AboutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Alex Robinson on 4/27/16.
+//  Copyright © 2016 Alex Robinson. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -37,26 +37,47 @@ class ViewController: UIViewController {
     @IBAction func showAlert() {
         let difference = abs(currentValue - targetValue)
         
-        let points = 100 - difference
+        var points = 100 - difference
+        
+        let title: String
+        
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "pretty good!"
+        } else {
+            title = "Not even close..."
+        }
         
         score += points
         
-        let message = "The value of the slider is: \(currentValue)" + "\n The target value is: \(targetValue)" + "\n You scored \(points) points!"
+        let message = "You scored \(points) points!"
         
-        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .Alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+                                                                                self.startNewRound()
+                                                                                self.updateLabels()
+                                                                            })
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
-        
-        startNewRound()
-        updateLabels()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
     }
     
     func startNewRound() {
@@ -70,6 +91,12 @@ class ViewController: UIViewController {
         targetLabel.text = "\(targetValue)"
         scoreLabel.text = "\(score)"
         roundLabel.text = "\(round)"
+    }
+    
+    func startNewGame() {
+        round = 0
+        score = 0
+        startNewRound()
     }
 }
 
